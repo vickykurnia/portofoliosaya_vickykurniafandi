4ab833dbc5725a1697baa6ce56a5ab26688ca250
vickykurnia <vickykurnia12@gmail.com>
2025-10-20 19:05:54 +0700

    hasilnew

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -17,7 +17,7 @@ export default function Home() {
       
       {/* FOTO */}
       <Image
-        src="/profile.png"
+        src="/foto.png!sw800"
         alt="Profile"
         width={120}
         height={120}
@@ -25,7 +25,7 @@ export default function Home() {
       />
 
       {/* HERO */}
-      <h1 className="text-4xl font-bold mb-2">Hi, I'm Fauzan 👋</h1>
+      <h1 className="text-4xl font-bold mb-2">Hi, I m vicky kurniafandi 👋</h1>
       <p className="text-gray-300 text-center max-w-md mb-12">
         Web Developer & Law Student who builds modern web applications.
       </p>
@@ -50,7 +50,7 @@ export default function Home() {
 
       {/* CONTACT */}
       <h2 className="text-2xl font-semibold mb-3">Contact</h2>
-      <p className="text-gray-400 mb-4">Let’s build something together!</p>
+      <p className="text-gray-400 mb-4">Let s build something together!</p>
       <a
         href="mailto:example@email.com"
         className="px-6 py-3 bg-gradient-to-r from-purple-500 to-pink-500 rounded-lg font-medium hover:opacity-90 transition"
